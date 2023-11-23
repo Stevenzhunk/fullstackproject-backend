2e2c2788989e0fbc159cbbfe0a52752fadeea3c4
Stevenzhunk <kikexgaz@gmail.com>
2023-11-23 17:10:57 -0300

    contacto añadido

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,23 +1,22 @@
-const index = (req,res) => {
-    res.render('index');
+const index = (req, res) => {
+  res.render('index');
 };
 
 const mainContact = (req, res) => {
-    res.send("<h1>Pagina Contacto</h1>");
-  };
+  res.render('contact');
+};
 
 const mainAbout = (req, res) => {
-    res.send("<h1>Pagina Acerca de Nosotros</h1>");
-  };
-  
-const mainFaqs = (req, res) => {
-    res.send("<h1>Pagina Faqs</h1>");
-  };
-  
+  res.send('<h1>Pagina Acerca de Nosotros</h1>');
+};
 
-module.exports= {
-    index,
-    mainAbout,
-    mainContact,
-    mainFaqs
+const mainFaqs = (req, res) => {
+  res.send('<h1>Pagina Faqs</h1>');
+};
+
+module.exports = {
+  index,
+  mainAbout,
+  mainContact,
+  mainFaqs,
 };
