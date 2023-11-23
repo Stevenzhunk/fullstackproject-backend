5203cad9e4989ea2ac84c6562731a95ba7a3adb0
Stevenzhunk <kikexgaz@gmail.com>
2023-11-23 16:48:37 -0300

    correcion header en cart, renderizado en shopitem

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,10 +1,45 @@
 const shopHome = (req, res) => {
-  res.send("<h1>Soy la pagina Shop!!!</h1>");
+  res.send('<h1>Soy la pagina Shop!!!</h1>');
 };
 
 const shopItem = (req, res) => {
   const { id } = req.params;
-  res.render("item", { itemId: id });
+  const productos = [
+    {
+      id: 1,
+      class: 'STAR WARS',
+      title: 'STORMTROOPER LIGHTSABER',
+      price: '$ 1799,99',
+      paymethod: '3 CUOTAS SIN INTERÉS',
+      imageBack: '/img/star-wars/trooper-box.webp',
+      imageFront: '/img/star-wars/trooper-1.webp',
+      altBack: 'BABY YODA BLUEBALL',
+      altFront: 'BABY YODA BLUEBALL',
+    },
+    {
+      id: 1,
+      class: 'POKEMON',
+      title: 'PIDGEOTTO',
+      price: '$ 1799,99',
+      paymethod: '3 CUOTAS SIN INTERÉS',
+      imageBack: '/img/pokemon/pidgeotto-box.webp',
+      imageFront: '/img/pokemon/pidgeotto-1.webp',
+      altBack: 'PIDGEOTTO',
+      altFront: 'PIDGEOTTO',
+    },
+    {
+      id: 1,
+      class: 'HARRY POTTER',
+      title: 'LUNA LOVEGOOD LION MASK',
+      price: '$ 1799,99',
+      paymethod: '3 CUOTAS SIN INTERÉS',
+      imageBack: '/img/harry-potter/luna-box.webp',
+      imageFront: '/img/harry-potter/luna-1.webp',
+      altBack: 'HARRY POTTER',
+      altFront: 'HARRY POTTER',
+    },
+  ];
+  res.render('item', { itemId: id, productos });
   console.log(`<h1>Soy el item de shop Nro : ${id} <h1>`);
 };
 
@@ -14,11 +49,11 @@ const addItem = (req, res) => {
 };
 
 const cartHome = (req, res) => {
-  res.render("cart");
+  res.render('cart');
 };
 
 const cartPost = (req, res) => {
-  res.send("<h1>Soy el post del carrito de compras</h1>");
+  res.send('<h1>Soy el post del carrito de compras</h1>');
 };
 
 module.exports = { shopHome, shopItem, addItem, cartHome, cartPost };
