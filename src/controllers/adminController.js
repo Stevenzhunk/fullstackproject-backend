afa85a945c91006da25f35af329e10d3e88f43ae
Stevenzhunk <kikexgaz@gmail.com>
2023-11-25 15:09:22 -0300

    Admin.ejs logica agregada

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -4,28 +4,142 @@
   res.render('the-view', { layout: 'specific-layout' });
 });
 */
-const path = require("path");
+
+// constante prueba para admin
+const productos = [
+  {
+    id: 1,
+    class: 'STAR WARS',
+    title: 'STORMTROOPER LIGHTSABER',
+    price: '$ 1799,99',
+    paymethod: '3 CUOTAS SIN INTERÉS',
+    imageBack: '/img/star-wars/trooper-box.webp',
+    imageFront: '/img/star-wars/trooper-1.webp',
+    altBack: 'BABY YODA BLUEBALL',
+    altFront: 'BABY YODA BLUEBALL',
+    barcode: 'STW0010001',
+  },
+  {
+    id: 2,
+    class: 'POKEMON',
+    title: 'PIDGEOTTO',
+    price: '$ 1799,99',
+    paymethod: '3 CUOTAS SIN INTERÉS',
+    imageBack: '/img/pokemon/pidgeotto-box.webp',
+    imageFront: '/img/pokemon/pidgeotto-1.webp',
+    altBack: 'PIDGEOTTO',
+    altFront: 'PIDGEOTTO',
+    barcode: 'STW0010002',
+  },
+  {
+    id: 3,
+    class: 'HARRY POTTER',
+    title: 'LUNA LOVEGOOD LION MASK',
+    price: '$ 1799,99',
+    paymethod: '3 CUOTAS SIN INTERÉS',
+    imageBack: '/img/harry-potter/luna-box.webp',
+    imageFront: '/img/harry-potter/luna-1.webp',
+    altBack: 'HARRY POTTER',
+    altFront: 'HARRY POTTER',
+    barcode: 'STW0010003',
+  },
+  {
+    id: 4,
+    class: 'STAR WARS',
+    title: 'STORMTROOPER LIGHTSABER',
+    price: '$ 1799,99',
+    paymethod: '3 CUOTAS SIN INTERÉS',
+    imageBack: '/img/star-wars/trooper-box.webp',
+    imageFront: '/img/star-wars/trooper-1.webp',
+    altBack: 'BABY YODA BLUEBALL',
+    altFront: 'BABY YODA BLUEBALL',
+    barcode: 'STW0010001',
+  },
+  {
+    id: 5,
+    class: 'POKEMON',
+    title: 'PIDGEOTTO',
+    price: '$ 1799,99',
+    paymethod: '3 CUOTAS SIN INTERÉS',
+    imageBack: '/img/pokemon/pidgeotto-box.webp',
+    imageFront: '/img/pokemon/pidgeotto-1.webp',
+    altBack: 'PIDGEOTTO',
+    altFront: 'PIDGEOTTO',
+    barcode: 'STW0010002',
+  },
+  {
+    id: 6,
+    class: 'HARRY POTTER',
+    title: 'LUNA LOVEGOOD LION MASK',
+    price: '$ 1799,99',
+    paymethod: '3 CUOTAS SIN INTERÉS',
+    imageBack: '/img/harry-potter/luna-box.webp',
+    imageFront: '/img/harry-potter/luna-1.webp',
+    altBack: 'HARRY POTTER',
+    altFront: 'HARRY POTTER',
+    barcode: 'STW0010003',
+  },
+  {
+    id: 7,
+    class: 'STAR WARS',
+    title: 'STORMTROOPER LIGHTSABER',
+    price: '$ 1799,99',
+    paymethod: '3 CUOTAS SIN INTERÉS',
+    imageBack: '/img/star-wars/trooper-box.webp',
+    imageFront: '/img/star-wars/trooper-1.webp',
+    altBack: 'BABY YODA BLUEBALL',
+    altFront: 'BABY YODA BLUEBALL',
+    barcode: 'STW0010001',
+  },
+  {
+    id: 8,
+    class: 'POKEMON',
+    title: 'PIDGEOTTO',
+    price: '$ 1799,99',
+    paymethod: '3 CUOTAS SIN INTERÉS',
+    imageBack: '/img/pokemon/pidgeotto-box.webp',
+    imageFront: '/img/pokemon/pidgeotto-1.webp',
+    altBack: 'PIDGEOTTO',
+    altFront: 'PIDGEOTTO',
+    barcode: 'STW0010002',
+  },
+  {
+    id: 9,
+    class: 'HARRY POTTER',
+    title: 'LUNA LOVEGOOD LION MASK',
+    price: '$ 1799,99',
+    paymethod: '3 CUOTAS SIN INTERÉS',
+    imageBack: '/img/harry-potter/luna-box.webp',
+    imageFront: '/img/harry-potter/luna-1.webp',
+    altBack: 'HARRY POTTER',
+    altFront: 'HARRY POTTER',
+    barcode: 'STW0010003',
+  },
+];
+
+const path = require('path');
 const adminHome = (req, res) => {
-  res.render("admin", {
-    layout: path.join(__dirname, "../views/layouts/layoutAdmin"),
+  res.render('admin', {
+    layout: path.join(__dirname, '../views/layouts/layoutAdmin'),
+    productos,
   });
 };
 
 const adminCreate = (req, res) => {
-  res.render("create", {
-    layout: path.join(__dirname, "../views/layouts/layoutAdmin"),
+  res.render('create', {
+    layout: path.join(__dirname, '../views/layouts/layoutAdmin'),
   });
 };
 
 const adminCreatePost = (req, res) => {
-  res.send("<h1>Soy el post de la Creacion del Admin</h1>");
+  res.send('<h1>Soy el post de la Creacion del Admin</h1>');
 };
 
 const adminEditGet = (req, res) => {
   const { id } = req.params;
   //como mando el parametro del item a editar
-  res.render("edit", {
-    layout: path.join(__dirname, "../views/layouts/layoutAdmin"),
+  res.render('edit', {
+    layout: path.join(__dirname, '../views/layouts/layoutAdmin'),
   });
 };
 
